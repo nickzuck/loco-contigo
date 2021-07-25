d8a5733e2baceeb92814f3a70edf8c42f80bf412
nickzuck-innov <nikhil.singh@innovaccer.com>
2021-07-25 11:33:19 +0530

    Remove unused code

--- transactions/transactions_api/urls.py
+++ transactions/transactions_api/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path("transaction", TransactionCreateView.as_view(), name = "create"),
-    # path("transaction/list", TransactionListView.as_view(), name = "list"),
-    # path("transaction/<int:txn_id>", TransactionUpdateView.as_view(), name = "update"),
     path("transaction/<int:txn_id>", transaction_detail, name = "transaction object op"),
     path("transaction/types/<str:type>", transaction_type, name = "transaction type op")
 ]
